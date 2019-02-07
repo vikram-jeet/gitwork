63c81c64a4b5884f71f133741aa4b4a75867ee20
Vikram Jeet <vikramjeet@centire.in>
2019-02-07 11:56:44 +0530

    commit wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+//define('DB_NAME', 'hendragym');
 define('DB_NAME', 'hendragym');
 
 /** MySQL database username */
